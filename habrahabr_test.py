e695f3a2de3d4bef952510d0c7dbd89a0ed6a1f4
kafeman <kafemanw@gmail.com>
2014-04-17 17:44:40 +0400

    Тесты, которые запускаются на всех пайтонах

--- habrahabr_test.py
+++ habrahabr_test.py
@@ -14,44 +14,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest2
+import unittest
 
 import habrahabr
 
 
-class ApiTestCase(unittest2.TestCase):
+class TestCase(unittest.TestCase):
+    def assert_is_instance(self, obj, cls):
+        if not isinstance(obj, cls):
+            msg = '%s is not an instance of %r' % (safe_repr(obj), cls)
+            self.fail(msg)
+
+
+class ApiTestCase(TestCase):
     def setUp(self):
         self.api = habrahabr.Api(token='', client='')
 
     def test_comments(self):
-        self.assertIsInstance(self.api.comments, habrahabr.CommentService)
+        self.assert_is_instance(self.api.comments, habrahabr.CommentService)
 
     def test_companies(self):
-        self.assertIsInstance(self.api.companies, habrahabr.CompanyService)
+        self.assert_is_instance(self.api.companies, habrahabr.CompanyService)
 
     def test_feed(self):
-        self.assertIsInstance(self.api.feed, habrahabr.FeedService)
+        self.assert_is_instance(self.api.feed, habrahabr.FeedService)
 
     def test_hubs(self):
-        self.assertIsInstance(self.api.hubs, habrahabr.HubService)
+        self.assert_is_instance(self.api.hubs, habrahabr.HubService)
 
     def test_polls(self):
-        self.assertIsInstance(self.api.polls, habrahabr.PollService)
+        self.assert_is_instance(self.api.polls, habrahabr.PollService)
 
     def test_posts(self):
-        self.assertIsInstance(self.api.posts, habrahabr.PostService)
+        self.assert_is_instance(self.api.posts, habrahabr.PostService)
 
     def test_search(self):
-        self.assertIsInstance(self.api.search, habrahabr.SearchService)
+        self.assert_is_instance(self.api.search, habrahabr.SearchService)
 
     def test_settings(self):
-        self.assertIsInstance(self.api.settings, habrahabr.SettingsService)
+        self.assert_is_instance(self.api.settings, habrahabr.SettingsService)
 
     def test_tracker(self):
-        self.assertIsInstance(self.api.tracker, habrahabr.TrackerService)
+        self.assert_is_instance(self.api.tracker, habrahabr.TrackerService)
 
     def test_users(self):
-        self.assertIsInstance(self.api.users, habrahabr.UserService)
+        self.assert_is_instance(self.api.users, habrahabr.UserService)
 
 
 if __name__ == '__main__':
